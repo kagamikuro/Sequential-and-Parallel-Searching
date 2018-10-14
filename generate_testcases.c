9e3fc91fdcdbce2529a274b64db0ed71d930fdd7
Tianbai Peng <40120405@ads.qub.ac.uk>
2018-10-14 20:15:41 +0100

    add somecode on generate text and pattern

--- generate_testcases.c
+++ generate_testcases.c
@@ -6,6 +6,47 @@
 #include<sys/stat.h>
 int main()
 {
+	int testNumber;
+	for(testNumber=0;testNumber<20;testNumber++){
+		char dirName[1000];
+		sprintf(dirName,"inputs/test%d",testNumber);
+		int status;
+		status = mkdir(dirName,S_IRWXU | S_IRWXG | S_IRWXO);
+		if(!status)
+			printf("Directory test%d created\n",testNumber);
+
+		long textLength;
+		if(testNumber>=0 && testNumber<4)
+			textLength = 100;
+		if(testNumber>=4 && testNumber<8)
+                        textLength = 10000;
+		if(testNumber>=8 && testNumber<12)
+                        textLength = 1000000;
+		if(testNumber>=12 && testNumber<16)
+                        textLength = 100000000;
+		if(testNumber>=16 && testNumber<20)
+                        textLength = 10000000000;
+
+		char textName[1000];
+		sprintf(textName,"inputs/test%d/text.txt",testNumber);
+		int handle_text;
+		char text[textLength];
+		handle_text = open(textName,O_CREAT|O_RDWR);
+		if(handle_text ==-1)
+                	printf("couldn't create tsxt for test%d\n",testNumber);
+		int ch = 'a';	
+		int i;
+		for(i=0;i<textLength-1;i++)				
+			text[i] = ch;
+		text[textLength-1] = 'b';
+		write(handle_text,text,textLength);	
+		
+	}	
+
+
+
+
+
 	int status;
 	status = mkdir("inputs/test2",S_IRWXU | S_IRWXG | S_IRWXO);
 	if(!status)
